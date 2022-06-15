162b8ab960dad6380af453529e5f200dc72e482f
Alice Boizet <alice.boizet@cirad.fr>
2022-06-15 14:22:19 +0200

    minor fix: add throws exception

--- src/fr/cirad/web/controller/rest/BrapiRestController.java
+++ src/fr/cirad/web/controller/rest/BrapiRestController.java
@@ -672,7 +672,7 @@ public class BrapiRestController implements ServletContextAware {
         @Authorization(value = "AuthorizationToken")}, value = "studyGerplasmList")
     @CrossOrigin
     @RequestMapping(value = "/{database:.+}" + URL_BASE_PREFIX + "/" + URL_STUDY_GERMPLASMS, method = RequestMethod.GET, produces = "application/json")
-    public Map<String, Object> studyGerplasmList(HttpServletRequest request, HttpServletResponse response, @PathVariable String database, @PathVariable(value = "studyDbId") int studyDbId, @RequestParam(required = false) Integer pageSize, @RequestParam(required = false) Integer page) {
+    public Map<String, Object> studyGerplasmList(HttpServletRequest request, HttpServletResponse response, @PathVariable String database, @PathVariable(value = "studyDbId") int studyDbId, @RequestParam(required = false) Integer pageSize, @RequestParam(required = false) Integer page) throws ObjectNotFoundException {
         MongoTemplate mongoTemplate = MongoTemplateManager.get(database);
         if (mongoTemplate == null) {
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
